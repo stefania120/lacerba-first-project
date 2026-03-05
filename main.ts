d27fb14bc2e20fa8d0d1caba792b537cac62da83
Stefania Formato <sformato95@gmail.com>
2026-03-05 21:21:30 +0100

    add for loop

--- main.ts
+++ main.ts
@@ -1,16 +1,19 @@
-let myLetVar: string= 'ciao';
-myLetVar = 'ciao2';
+// let myLetVar: string= 'ciao';
+// myLetVar = 'ciao2';
 
-const myConstant = 'ciao';
+// const myConstant = 'ciao';
 
-let myNumber: number = 0.123;
+// let myNumber: number = 0.123;
 
-let myBoolean: boolean = false;
+// let myBoolean: boolean = false;
 
-let myNumber2: number | undefined; 
-console.log('myNumber2 = ', myNumber2); 
+// let myNumber2: number | undefined; 
+// console.log('myNumber2 = ', myNumber2); 
 
-let myUndefinedVar: undefined;// undefined
+// let myUndefinedVar: undefined;// undefined
 
 
-
+console.log('Hello Computing square of value');
+for (let x = 1; x < 100; x++) {
+    console.log(`\t${x}^2 = ${x * x}`);
+}
