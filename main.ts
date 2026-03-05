99daf5928a6e892781e76e9d8aa1b90854366b0c
Stefania Formato <sformato95@gmail.com>
2026-03-05 22:29:05 +0100

    add if and switch

--- main.ts
+++ main.ts
@@ -1,10 +1,28 @@
-const numbers: number[] = [1, 2, 3, 4, 5];
+const x : number= 10;
+ switch (x) {
+    case 1:
+        console.log('x vale 1');
+        break;
+    case 2:
+        console.log('x vale 2');
+        break;
+    case 10:
+        console.log('x vale 10');
+        break;
+    default:
+        console.log('non conosco il valore di x');
+        break;   
+ }
 
-let sum = 0;
-for(const n of numbers) {
-    sum += n;
+ console.log('------------------');
+ 
+
+if (x === 1) {
+    console.log('x vale 1');
+} else if (x === 2) {
+    console.log('x vale 2');
+} else if (x === 10) {
+    console.log('x vale 10');
+} else {
+    console.log('non conosco il valore di x');
 }
-
-const avg = sum / numbers.length;
-console.log('Somma: ', sum);
-console.log('Media: ', avg);
