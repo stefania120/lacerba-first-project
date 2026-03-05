ca0fd25e6bf895aa9d45591434d1276afdcb9edb
Stefania Formato <sformato95@gmail.com>
2026-03-05 22:12:19 +0100

    Challenge of sum and average

--- main.ts
+++ main.ts
@@ -1,35 +1,10 @@
-// let myLetVar: string= 'ciao';
-// myLetVar = 'ciao2';
+const numbers: number[] = [1, 2, 3, 4, 5];
 
-// const myConstant = 'ciao';
-
-// let myNumber: number = 0.123;
-
-// let myBoolean: boolean = false;
-
-// let myNumber2: number | undefined; 
-// console.log('myNumber2 = ', myNumber2); 
-
-// let myUndefinedVar: undefined;// undefined
-
-
-console.log('Hello Computing square of value');
-for (let x = 1; x < 100; x++) {
-    console.log(`\t${x}^2 = ${x * x}`);
+let sum = 0;
+for(const n of numbers) {
+    sum += n;
 }
 
-console.log(' ---- ');
-
-const numbers = [2, 6, 12, 43];
-for (const x of numbers) {
-    console.log(`\t${x}^2 = ${x * x}`);
-    
-}
-console.log(' ---- ');
-
-let x = 1;
-while (x < 10) {
-    console.log(`\t${x}^2 = ${x * x}`);
-    x++;
-}
-
+const avg = sum / numbers.length;
+console.log('Somma: ', sum);
+console.log('Media: ', avg);
