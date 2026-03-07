fd4d1bdee5e5c93c7d808778dc523d4022bc6635
Stefania Formato <sformato95@gmail.com>
2026-03-08 00:16:02 +0100

    add greeting function with union types

--- main.ts
+++ main.ts
@@ -1,18 +1,16 @@
-function hello(name: string): string {
-    console.log( "Hello, world!" );
-    console.log(`hello from, ${name}!`); 
-    return `questa è la stringa di ritorno`;
+function greeting(name: string, lang: 'en' | 'it' | 'es' | 'fr' = 'it'){
+
+const greetMap = {
+    en: 'Hello',
+    it: 'Ciao',
+    es: 'Hola',
+    fr: 'Bonjour',
+};
+
+const greetingWord = greetMap[lang];
+
+return `${greetingWord} ${name}`;
 }
 
-hello('Ludovico');
-hello('Marco');
-
-console.log(`\n-----------------------------\n`);
-
-
-function greeting(name: string) {
-    return `Ciao ${name}`;
-}
-
-console.log(greeting('Ludovico'));
-console.log(greeting('Marco'));
+console.log(greeting('Ludovico', 'en'));
+console.log(greeting('Marco' ));
